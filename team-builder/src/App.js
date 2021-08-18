974bdb7665886a06edec7467bb376a82d0e9554a
Nelson Maldonado <play2nino@gmail.com>
2021-08-18 00:13:59 -0700

    team members name label added

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -4,20 +4,11 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <h1> Workingfine</h1>
+      <h1> React Forms</h1>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>App by Nelson Maldonado</p>
       </header>
     </div>
   )
--- team-builder/src/index.js
+++ team-builder/src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
+const teamList = [
+  { teamName: "Jason" },
+  { teamName: "Steve" },
+  { teamName: "Ryan" },
+]
+
+function SimpleForm() {
+  const [formValues, setFormValues] = useState({ teamName: "" })
+  const [person, setPerson] = useState(teamList)
+
+  const change = (event) => {
+    const { name, value } = event.target
+
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const submit = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      teamName: formValues.teamName,
+    }
+
+    setPerson(person.concat(newPerson))
+    setFormValues({ teamName: "" })
+  }
+
+  return (
+    <div className="container">
+      <h1>Team Members Form</h1>
+      <form onSubmit={submit}>
+        <label>
+          Members Name
+          <input
+            type="text"
+            name="teamMember"
+            value={formValues.teamName}
+            onChange={change}
+          />
+        </label>
+        <input type="submit" value="CREATE YOUR TEAM" />
+      </form>
+    </div>
+  )
+}
 ReactDOM.render(
   <React.StrictMode>
+    <SimpleForm />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
